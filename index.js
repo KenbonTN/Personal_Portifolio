d9cf04b4a9a6e2a035fdc151b1f463f0b252a283
Kenbon Teshome <kenbonnurgi@gmail.com>
2022-11-24 12:38:15 +0300

    Added an updated javaScript file

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const closeBtn = document.querySelector(".image");
-closeBtn.addEventListener("click", ()=>{
-  document.querySelector(".mobile-menu").setAttribute("style", "display:none");
-})
+const closeBtn = document.querySelector('.image');
+const mobileNavItem = document.querySelectorAll('.mobile-item');
+const humburgerIcon = document.querySelector('.toggler-img');
 
-const humburgerIcon = document.querySelector(".toggler-img");
-humburgerIcon.addEventListener("click", ()=>{
-  document.querySelector(".mobile-menu").setAttribute("style", "display:flex");
-})
+function showMenu() {
+  document.querySelector('.mobile-menu').setAttribute('style', 'display:flex');
+  document.querySelector('.grid-container').setAttribute('style', 'display:none');
+  document.querySelector('.footer').setAttribute('style', 'display:none');
+  document.querySelector('.about-me').setAttribute('style', 'display:none');
+}
 
-const mobileNavItem =document.querySelectorAll(".mobile-item");
-  mobileNavItem.forEach((item) => {
-  item.addEventListener('click', () => {
-    document.querySelector('.mobile-menu').setAttribute("style", "display:none");
-  });
-})
+function hideMenu() {
+  document.querySelector('.mobile-menu').setAttribute('style', 'display:none');
+  document.querySelector('.grid-container').setAttribute('style', 'display:grid');
+  document.querySelector('.footer').setAttribute('style', 'display:flex');
+  document.querySelector('.about-me').setAttribute('style', 'display:block');
+}
+
+// click on the hamburger icon
+humburgerIcon.addEventListener('click', showMenu);
+
+// click on the close icon
+closeBtn.addEventListener('click', hideMenu);
+
+mobileNavItem.forEach((item) => {
+  item.addEventListener('click', hideMenu);
+});
