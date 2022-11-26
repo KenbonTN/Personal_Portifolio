4ccef02da3b075a3be1432e6c2d5a0dc068845b9
Kenbon Teshome <kenbonnurgi@gmail.com>
2022-11-26 17:06:11 +0300

    Complete form validation

---
+++ js file/validation.js
@@ -0,0 +1,9 @@
+document.getElementById('contact').addEventListener('submit', (event) => {
+    const email = document.getElementById('email').value;
+    if (email !== email.toLowerCase()) {
+      event.preventDefault();
+      document.querySelector('.error').innerHTML = 'Your email address should be in lowercase';
+      event.target.reset();
+    }
+  });
+  
